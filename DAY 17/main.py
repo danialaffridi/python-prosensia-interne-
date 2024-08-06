7a75f7e23a2e0c2527839f0038f00b7b4926e219
danialaffridi <162950765+danialaffridi@users.noreply.github.com>
2024-08-06 00:56:57 -0700

    Add files via upload

---
+++ DAY 17/main.py
@@ -0,0 +1,47 @@
+# QUESTION 01
+fruits = ['apple', 'banana', 'cherry']
+print(fruits)
+
+# QUESTION 02
+numbers = [10, 20, 30, 40, 50]
+print(numbers[1])
+print(numbers[3])
+
+# QUESTION 03
+colors = ['red', 'green', 'blue', 'yellow', 'purple']
+print(colors[:3])
+
+# QUESTION 04
+animals = ['cat', 'dog', 'rabbit']
+animals[2] = 'hamster'
+print(animals)
+
+# QUESTION 05
+zoo = ['lion', 'tiger', 'bear']
+zoo.append('elephant')
+print(zoo)
+
+# QUESTION 06
+fruits = ['apple', 'banana', 'cherry']
+fruits.insert(1, 'grape')
+print(fruits)
+
+# QUESTION 07
+fruits = ['apple', 'banana', 'cherry']
+fruits.remove('banana')
+print(fruits)
+
+# QUESTION 08
+languages = ['Python', 'Java', 'C++', 'JavaScript']
+print(len(languages))
+
+# QUESTION 09
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+concatenated_list = list1 + list2
+print(concatenated_list)
+
+# QUESTION 10
+numbers = [1, 2, 3, 4, 5]
+squares = [number ** 2 for number in numbers]
+print(squares)
